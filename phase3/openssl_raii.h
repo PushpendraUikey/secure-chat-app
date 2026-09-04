8235f9945ba353223c150e99477afef5645fad11
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2026-09-04 17:03:49 +0530

    Phase 3: RAII wrappers for X509, EVP_PKEY, and BIO pointers

--- phase3/openssl_raii.h
+++ phase3/openssl_raii.h
@@ -3,6 +3,8 @@
 
 #include <openssl/bn.h>
 #include <openssl/evp.h>
+#include <openssl/x509.h>
+#include <openssl/bio.h>
 #include <memory>
 
 // Deleter-wrapped unique_ptr aliases for OpenSSL's C-style handles.
@@ -21,5 +23,16 @@ using EVP_CIPHER_CTX_ptr = std::unique_ptr<EVP_CIPHER_CTX, EVP_CIPHER_CTX_Delete
 
 struct EVP_MD_CTX_Deleter { void operator()(EVP_MD_CTX* ctx) const { EVP_MD_CTX_free(ctx); } };
 using EVP_MD_CTX_ptr = std::unique_ptr<EVP_MD_CTX, EVP_MD_CTX_Deleter>;
+
+// --- New for Phase 3 (PKI) ---
+
+struct X509_Deleter { void operator()(X509* x) const { X509_free(x); } };
+using X509_ptr = std::unique_ptr<X509, X509_Deleter>;
+
+struct EVP_PKEY_Deleter { void operator()(EVP_PKEY* k) const { EVP_PKEY_free(k); } };
+using EVP_PKEY_ptr = std::unique_ptr<EVP_PKEY, EVP_PKEY_Deleter>;
+
+struct BIO_Deleter { void operator()(BIO* b) const { BIO_free(b); } };
+using BIO_ptr = std::unique_ptr<BIO, BIO_Deleter>;
 
 #endif
